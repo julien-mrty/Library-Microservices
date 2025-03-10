7e47301c40f92495d37be003545055461cc12862
TZ-Boomer <julienmarty0819@gmail.com>
2025-03-10 17:07:32 +0100

    Pour Davidou

--- book-service/server.js
+++ book-service/server.js
@@ -1,4 +1,12 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+
+// Determine the environment
+const envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev';
+
+// Load the environment file
+dotenv.config({ path: envFile });
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
